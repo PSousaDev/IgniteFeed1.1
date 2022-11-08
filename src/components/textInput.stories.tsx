ae49e2a0b7c4e519350e75d106201e7e6aa2e99b
pedro silva <rochapedro2009@hotmail.com>
2022-11-07 21:03:26 -0300

    addWithoutIcon

--- src/components/textInput.stories.tsx
+++ src/components/textInput.stories.tsx
@@ -1,23 +1,17 @@
+import { Meta, StoryObj } from "@storybook/react";
 import { Envelope } from "phosphor-react";
-import { StoryObj, Meta } from "@storybook/react";
-import {
-  TextInput,
-  TextInputInputProps,
-  textInputRootProps,
-} from "./textInput";
-export default {
-  title: "components/TextInput",
-  component: TextInput.root,
+import { TextInput, textInputRootProps } from "./textInput";
 
+export default {
+  title: "Components/TextInput",
+  component: TextInput.root,
   args: {
-    children: (
-      <>
-        <TextInput.icon>
-          <Envelope />
-        </TextInput.icon>
-        <TextInput.input placeholder="Type your best email" />,
-      </>
-    ),
+    children: [
+      <TextInput.icon>
+        <Envelope />
+      </TextInput.icon>,
+      <TextInput.input placeholder="Type your e-mail address" />,
+    ],
   },
   argTypes: {
     children: {
@@ -26,6 +20,12 @@ export default {
       },
     },
   },
-} as Meta<TextInputInputProps>;
+} as Meta<textInputRootProps>;
 
 export const Default: StoryObj<textInputRootProps> = {};
+
+export const WithoutIcon: StoryObj<textInputRootProps> = {
+  args: {
+    children: <TextInput.input placeholder="Type your e-mail address" />,
+  },
+};
